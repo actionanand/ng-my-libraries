31a3dd651e944a7334544819cc5cc6fd0725d7d2
Anand raja <meetanandns@gmail.com>
2022-06-19 19:43:28 +0000

    initial commit

---
+++ apps/ar-input/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-my-libraries-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ar-input';
+}
