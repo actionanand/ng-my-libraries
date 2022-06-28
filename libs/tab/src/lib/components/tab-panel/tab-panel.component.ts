8344c44dcd85452e302f3ffe5e718ab9f88a2cbb
Anand raja <meetanandns@gmail.com>
2022-06-28 18:22:33 +0000

    tab lib skeleton - ready

---
+++ libs/tab/src/lib/components/tab-panel/tab-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-ar-tab-panel',
+  templateUrl: './tab-panel.component.html',
+  styleUrls: ['./tab-panel.component.scss'],
+})
+export class TabPanelComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- libs/tab/src/lib/tab.module.ts
+++ libs/tab/src/lib/tab.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TabComponent } from './components/tab/tab.component';
+import { TabPanelComponent } from './components/tab-panel/tab-panel.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [TabComponent, TabPanelComponent],
 })
 export class TabModule {}
