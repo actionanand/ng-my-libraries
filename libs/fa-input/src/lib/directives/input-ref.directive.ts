eb5856877ac70fa9edbb8f5a457d6bd48c0adb9d
Anand raja <meetanandns@gmail.com>
2022-06-28 20:08:08 +0000

    disbling lint error

--- libs/fa-input/src/lib/directives/input-ref.directive.ts
+++ libs/fa-input/src/lib/directives/input-ref.directive.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @angular-eslint/component-selector */
+/* eslint-disable @angular-eslint/directive-selector */
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Directive, HostListener } from '@angular/core';
 
