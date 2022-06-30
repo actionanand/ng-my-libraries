f1daf51f99b2b28eef5678c5a63bd218678f54a5
Anand raja <meetanandns@gmail.com>
2022-06-30 18:16:20 +0000

    structural dir added

--- libs/modal/src/lib/directives/modal-open-on-click.directive.ts
+++ libs/modal/src/lib/directives/modal-open-on-click.directive.ts
@@ -1,12 +1,20 @@
 /* eslint-disable @angular-eslint/directive-selector */
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[ngArModalOpenOnClick]'
 })
 export class ModalOpenOnClickDirective {
 
-  constructor() { }
+  constructor(private templateRef: TemplateRef<unknown>, private viewContainer: ViewContainerRef) { }
+
+  @Input()
+    set ngArModalOpenOnClick(el: HTMLBaseElement | any){
+      el.addEventListener('click', () => {
+        this.viewContainer.clear();
+        this.viewContainer.createEmbeddedView(this.templateRef);
+      });
+    }
 
 }
