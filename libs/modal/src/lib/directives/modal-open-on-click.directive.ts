26c5810b81b8ea6f65e62e66171d1c1e12fec880
Anand raja <meetanandns@gmail.com>
2022-06-30 15:38:42 +0000

    basic skeleton added

---
+++ libs/modal/src/lib/directives/modal-open-on-click.directive.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @angular-eslint/directive-selector */
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[ngArModalOpenOnClick]'
+})
+export class ModalOpenOnClickDirective {
+
+  constructor() { }
+
+}
--- libs/modal/src/lib/modal.module.ts
+++ libs/modal/src/lib/modal.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ModalComponent } from './modal/modal.component';
+import { ModalOpenOnClickDirective } from './directives/modal-open-on-click.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ModalComponent],
+  declarations: [ModalComponent, ModalOpenOnClickDirective],
+  exports: [ModalOpenOnClickDirective]
 })
 export class ModalModule {}
