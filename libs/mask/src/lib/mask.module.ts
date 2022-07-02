0c9227935a7b3aa6967bdd7aaa4e32aec15ab344
Anand raja <meetanandns@gmail.com>
2022-07-02 13:27:57 +0000

    mask skeleton added

--- libs/mask/src/lib/mask.module.ts
+++ libs/mask/src/lib/mask.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaskDirective } from './directives/mask.directive';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    MaskDirective
+  ],
 })
 export class MaskModule {}
