59865fcab77b22b892b39979ec41db694df20934
Anand raja <meetanandns@gmail.com>
2022-07-03 09:18:05 +0000

    home page works

--- apps/ar-input/src/app/pages/home/home.component.ts
+++ apps/ar-input/src/app/pages/home/home.component.ts
@@ -1,4 +1,9 @@
+/* eslint-disable @angular-eslint/component-selector */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, OnInit } from '@angular/core';
+
+import { ICON_TYPE, ICON_NAME } from '@ng-ar/fa-input';
 
 @Component({
   selector: 'ng-my-libraries-home',
@@ -7,6 +12,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   constructor() {}
+
+  typeSolid = ICON_TYPE.SOLID;
+  typeReg = ICON_TYPE.REGULAR;
+  typeBrand = ICON_TYPE.BRAND;
+
+  iconEmail = ICON_NAME.REG_SOLID.ENVELOPE;
+  iconStripe = ICON_NAME.BRAND.STRIPE;
+  iconCc = ICON_NAME.REG_SOLID.CREDIT_CARD;
 
   ngOnInit(): void {}
 }
